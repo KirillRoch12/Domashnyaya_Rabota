8bf9d9992adac1243646c0e6a99ddec045c96d55
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2022-12-29 00:18:48 +0500

    Задание 23

---
+++ Задание 23 (28.12.2022).cs
@@ -0,0 +1,21 @@
+﻿namespace Задача_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n,k;
+
+            Console.WriteLine("Сколько кубов от 1 до N нужно вычислить :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            double i = 1;
+            while (n >= i)
+            {
+                k = Math.Pow(i,3);
+                Console.WriteLine($"Куб числа {i} равен {k}");
+                i = i + 1;
+            }
+        }
+    }
+}
