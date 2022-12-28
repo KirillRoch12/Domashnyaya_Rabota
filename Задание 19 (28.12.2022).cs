9dd95263274548dd53b00bca1e6755902e215784
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2022-12-28 23:59:17 +0500

    задание 19

---
+++ Задание 19 (28.12.2022).cs
@@ -0,0 +1,38 @@
+﻿namespace Задача_19
+{
+
+    public static class Extensions
+    {
+        public static string Reverse(this string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число для проверки на палиндром :");
+                string str = Console.ReadLine();
+
+                string reverse = str.Reverse();
+
+                if (str == reverse)
+                {
+                    Console.WriteLine($"число {str} является палиндромом");
+                }
+                else
+                {
+                    Console.WriteLine($"число {str} не является палиндромом");
+                }
+            }
+        }
+    }
+}
+        
+    
