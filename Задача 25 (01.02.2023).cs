2de5803e0a01d010e10838be7847de7bbcdb68c0
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-01 18:29:24 +0500

    Задача 25

---
+++ Задача 25 (01.02.2023).cs
@@ -0,0 +1,32 @@
+﻿namespace Задание_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int a, b, c, i;
+
+                Console.WriteLine("Введите возводимое число : ");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите степень : ");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                i = 0;
+                c = 1;
+
+                while (i < b)
+                {
+                    c = c * a;
+                    i = i + 1;
+                }
+
+                Console.WriteLine($"Вторая цифра этого числа : {c}");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
