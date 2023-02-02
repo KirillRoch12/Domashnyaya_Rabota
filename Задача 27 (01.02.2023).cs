d7705642574b2ad488e5e3def19e134d37b58032
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-02 05:16:45 +0500

    Задание 27  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Задача 27 (01.02.2023).cs
@@ -0,0 +1,26 @@
+﻿namespace Задача_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+         {
+            while (true)
+            {
+                
+
+                Console.WriteLine("Введите число : ");
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                int S = 0;
+
+                while (N >= 1)
+                {
+                    S+= N % 10;
+                    N = (N - N % 10) / 10;
+                }
+                Console.WriteLine($"Сумма цифр данного числа равна : {S}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
