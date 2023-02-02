b6b940323c0bbf2a6c2a52c198cec52c5957e6aa
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-02 05:39:31 +0500

    Задание 29

---
+++ Задача 29 (01.02.2023).cs
@@ -0,0 +1,21 @@
+﻿namespace Задание_29
+{
+    internal class Program
+    {
+        static void Main(string[] all)
+        {
+
+            Console.Write("Введите любое число : ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[A];
+
+            for (int i = 0; i < A; i++)
+            {
+                array[i] = new Random().Next(A + 1);
+                Console.Write(array[i] + " , ");
+            }
+            
+        }
+    }
+}
