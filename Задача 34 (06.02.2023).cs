0082ad0181b89d16c647004fe8f7f3d575a15d6d
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-06 13:30:33 +0500

    Задание 34

---
+++ Задача 34 (06.02.2023).cs
@@ -0,0 +1,32 @@
+﻿namespace Задание_34
+{
+    internal class Program
+    {
+        static void Main(string[] all)
+        {
+
+            Console.Write("Введите любое число : ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[A];
+            int k = 0;
+
+
+            for (int i = 0; i < A; i++)
+            {
+                array[i] = 100 + new Random().Next(999);
+
+                if (array[i] % 2 == 0)
+                {
+                    k++;
+                }
+                Console.Write(array[i] + " , ");
+            }
+            Console.Write($"Количество чётных чисел в массиве :{k}");
+
+
+
+
+        }
+    }
+}
