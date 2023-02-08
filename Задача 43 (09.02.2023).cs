bbec81510ceaf87656abdfa233216a7387508cdd
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-09 01:43:28 +0500

    Задача 43

---
+++ Задача 43 (09.02.2023).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задача_43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первые кардинаты : ");
+            var b1 = Convert.ToInt32(Console.ReadLine());
+            var k1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите вторые кардинаты : ");
+            var b2 = Convert.ToInt32(Console.ReadLine());
+            var k2 = Convert.ToInt32(Console.ReadLine());
+
+            double x;
+            x = ((double)(b2 - b1) / (k1 - k2));
+
+            Console.WriteLine($"Точка пересечения : {x} {k1 * x +b1}") ;
+
+
+        }
+    }
+}
