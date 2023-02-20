60690b43bf0348eba3f0cc3cd4de9fc341b8cd5b
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-20 13:00:57 +0500

    Задание 68

---
+++ Задача 68 (20.02.2023).cs
@@ -0,0 +1,42 @@
+﻿namespace Задание_68
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int Akkerman(int m, int n)
+                {
+                    if (m == 0)
+                    {
+                        return n + 1;
+                    }
+                    else if (n == 0 && m > 0)
+                    {
+                        return Akkerman(m - 1, 1);
+                    }
+                    else
+                    {
+                        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+                    }
+                }
+
+                Console.WriteLine("Введите число M: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите число N: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (m > 0 && n > 0)
+                {
+                    Console.WriteLine(Akkerman(m, n));
+                }
+                else
+                {
+                    Console.WriteLine(" Данные числа являются отрицательными ");
+                }
+            }
+        }
+    }
+}
