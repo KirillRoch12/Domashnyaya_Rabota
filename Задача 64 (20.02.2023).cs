e2e20edf9b7f68ad2d76bbf01cf23568d9c75f2b
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-20 13:16:33 +0500

    Задание 64

---
+++ Задача 64 (20.02.2023).cs
@@ -0,0 +1,25 @@
+﻿namespace Задание_64
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            void OtMDoN(int N, int M)
+            {
+                if (N == M) return;
+                {
+                    OtMDoN(N, ++M);
+                    
+                    Console.Write(M + " ");
+                }
+            }
+
+            Console.Write(" Задайте значение N : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int M = 1;
+
+            OtMDoN(N, M - 1);      
+        }
+    }
+}
