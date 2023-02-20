640df69aa642ce95e5ba9b7a76e8230c79248970
roch_hamm <65013992+KirillRoch12@users.noreply.github.com>
2023-02-20 13:16:46 +0500

    Задание 66

---
+++ Задача 66 (20.02.2023).cs
@@ -0,0 +1,30 @@
+﻿namespace Задание_66
+{
+    internal class Program
+    {
+            static void Main()
+            {
+                int summ = 0;
+                void OtMDoN(int N, int M)
+                {
+                    if (N == M) return;
+                    {
+                        OtMDoN(N, ++M);
+                        summ += M;
+                        
+                    }
+                }
+
+                Console.Write(" Задайте значение M : ");
+                int M = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write(" Задайте значение N : ");
+                int N = Convert.ToInt32(Console.ReadLine());
+
+
+                OtMDoN(N, M - 1);
+                Console.WriteLine(summ);
+            }
+    }
+}
+  
